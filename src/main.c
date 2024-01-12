6d1d107033828df69d47bd8d9341f4316bab551e
Victor Garcia <victorgf2011@gmail.com>
2024-01-12 16:48:35 +0100

    feat: update

--- src/main.c
+++ src/main.c
@@ -18,7 +18,7 @@
 
 char *buffer = "\0";
 
-void panic(int status, const char *format_message, ...) {
+void Panic(int status, const char *format_message, ...) {
   va_list args;
   va_start(args, format_message);
   vfprintf(stderr, format_message, args);
@@ -43,33 +43,24 @@ void String_Push_Char(char **str, char concat) {
   *str = new_str;
 }
 
-void type_in_buffer(char key) {
-  // int len = strlen(buffer);
-  // char *new_buffer = malloc(len + 1);
-  // strncpy(new_buffer, buffer, len);
-  // new_buffer[len] = key;
-  // new_buffer[len + 1] = '\0';
-  // buffer = new_buffer;
+void Type_In_Buffer(char key) { String_Push_Char(&buffer, key); }
 
-  String_Push_Char(&buffer, key);
-}
-
-int subrender_text(SDL_Renderer *renderer, TTF_Font *font,
-                   const char *wantedText, SDL_Color textColor,
-                   SDL_Color textBackgroundColor, int x, int y) {
+int Render_Paragraph(SDL_Renderer *renderer, TTF_Font *font,
+                     const char *wantedText, SDL_Color textColor,
+                     SDL_Color textBackgroundColor, int x, int y) {
   SDL_Texture *text = NULL;
   SDL_Rect textRect;
 
   SDL_Surface *textSurface =
       TTF_RenderText_Shaded(font, wantedText, textColor, textBackgroundColor);
   if (!textSurface) {
-    panic(1, "Unable to render text surface!\nSDL2_ttf Error: %s\n",
+    Panic(1, "Unable to render text surface!\nSDL2_ttf Error: %s\n",
           TTF_GetError());
   }
 
   text = SDL_CreateTextureFromSurface(renderer, textSurface);
   if (!text) {
-    panic(1, "Unable to create texture from rendered text!\nSDL2 Error: %s\n",
+    Panic(1, "Unable to create texture from rendered text!\nSDL2 Error: %s\n",
           SDL_GetError());
   }
 
@@ -87,7 +78,7 @@ int subrender_text(SDL_Renderer *renderer, TTF_Font *font,
   return 1;
 }
 
-int render_text(SDL_Renderer *renderer, TTF_Font *font,
+int Render_Text(SDL_Renderer *renderer, TTF_Font *font,
                 const char *wantedText) {
   SDL_Color textColor = {0x00, 0x00, 0x00, 0xFF};
   SDL_Color textBackgroundColor = {0xFF, 0xFF, 0xFF, 0xFF};
@@ -102,8 +93,8 @@ int render_text(SDL_Renderer *renderer, TTF_Font *font,
 
     if (current_character == '\n') {
       if (strlen(current_line) > 0) {
-        subrender_text(renderer, font, current_line, textColor,
-                       textBackgroundColor, 0, line_number * 40);
+        Render_Paragraph(renderer, font, current_line, textColor,
+                         textBackgroundColor, 0, line_number * 40);
       }
 
       current_line = "";
@@ -111,52 +102,11 @@ int render_text(SDL_Renderer *renderer, TTF_Font *font,
     } else if (current_character != '\0') {
       String_Push_Char(&current_line, current_character);
     } else if (strlen(current_line) > 0) {
-      subrender_text(renderer, font, current_line, textColor,
-                     textBackgroundColor, 0, line_number * 40);
+      Render_Paragraph(renderer, font, current_line, textColor,
+                       textBackgroundColor, 0, line_number * 40);
     }
   }
 
-  // char *line = malloc(strlen(wantedText));
-  // int line_number = 0;
-  //
-  // for (int i = 0; i < strlen(wantedText); i++) {
-  //   printf("%s\n", line);
-  //
-  //   if (wantedText[i] == '\0') {
-  //     subrender_text(renderer, font, line, textColor, textBackgroundColor, 0,
-  //                    line_number * 40);
-  //   } else {
-  //     printf("adding %c\n", wantedText[i]);
-  //     // int len = strlen(line);
-  //     // char *new_line = malloc(len + 1);
-  //     // strncpy(new_line, line, len);
-  //     // new_line[len] = wantedText[i];
-  //     // new_line[len + 1] = '\0';
-  //     // line = new_line;
-  //   }
-  //
-  //   // if (wantedText[i] != '\n' && wantedText[i] != '\0') {
-  //   //   int len = strlen(line);
-  //   //   char *new_line = malloc(len + 1);
-  //   //   strncpy(new_line, line, len);
-  //   //   new_line[len] = wantedText[i];
-  //   //   new_line[len + 1] = '\0';
-  //   //   line = new_line;
-  //   // } else if (wantedText[i] == '\n') {
-  //   //   line = malloc(strlen(wantedText));
-  //   //   line_number++;
-  //   //
-  //   //   subrender_text(renderer, font, line, textColor, textBackgroundColor,
-  //   0,
-  //   //                  line_number * 40);
-  //   // } else if (wantedText[i] == '\0') {
-  //   //   subrender_text(renderer, font, line, textColor, textBackgroundColor,
-  //   0,
-  //   //                  line_number * 40);
-  //   // }
-  // }
-  //
-  // free(line);
   return 1;
 }
 
@@ -165,7 +115,7 @@ int main(int argc, char *argv[]) {
   (void)argv;
 
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    panic(1, "SDL2 could not be initialized!\nSDL2 Error: %s\n",
+    Panic(1, "SDL2 could not be initialized!\nSDL2 Error: %s\n",
           SDL_GetError());
   }
 
@@ -173,7 +123,7 @@ int main(int argc, char *argv[]) {
 
 #if defined linux && SDL_VERSION_ATLEAST(2, 0, 8)
   if (!SDL_SetHint(SDL_HINT_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR, "0")) {
-    panic(0, "SDL can not disable compositor bypass!\n");
+    Panic(0, "SDL can not disable compositor bypass!\n");
   }
 #endif
 
@@ -181,18 +131,18 @@ int main(int argc, char *argv[]) {
       SDL_CreateWindow("edi", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
                        SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
   if (!window) {
-    panic(1, "Window could not be created!\nSDL_Error: %s\n", SDL_GetError());
+    Panic(1, "Window could not be created!\nSDL_Error: %s\n", SDL_GetError());
   }
 
   SDL_Renderer *renderer =
       SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
   if (!renderer) {
-    panic(1, "Renderer could not be created!\nSDL_Error: %s\n", SDL_GetError());
+    Panic(1, "Renderer could not be created!\nSDL_Error: %s\n", SDL_GetError());
   }
 
   TTF_Font *font = TTF_OpenFont(FONT_PATH, 40);
   if (!font) {
-    panic(1, "Unable to load font: '%s'!\nTTF_Error: %s\n", FONT_PATH,
+    Panic(1, "Unable to load font: '%s'!\nTTF_Error: %s\n", FONT_PATH,
           TTF_GetError());
   }
 
@@ -215,7 +165,7 @@ int main(int argc, char *argv[]) {
       break;
 
     case SDL_TEXTINPUT:
-      type_in_buffer(e.text.text[0]);
+      Type_In_Buffer(e.text.text[0]);
       break;
 
     case SDL_QUIT:
@@ -242,7 +192,7 @@ int main(int argc, char *argv[]) {
         break;
       }
       case SDLK_RETURN: {
-        type_in_buffer('\n');
+        Type_In_Buffer('\n');
         break;
       }
       default: {
@@ -256,7 +206,7 @@ int main(int argc, char *argv[]) {
     SDL_RenderClear(renderer);
 
     if (buffer[0] != '\0') {
-      render_text(renderer, font, buffer);
+      Render_Text(renderer, font, buffer);
     }
 
     SDL_RenderPresent(renderer);

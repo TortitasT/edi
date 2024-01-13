a1afd4cd9ba83bdae4201c4455c3d3d9a844d364
Victor Garcia <victorgf2011@gmail.com>
2024-01-13 11:31:03 +0100

    feat: skip newlines wip

--- src/main.c
+++ src/main.c
@@ -18,6 +18,7 @@
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
 #define CHAR_HEIGHT 20
+#define CHAR_WIDTH 12
 
 #define FONT_PATH "assets/fonts/GoMonoNerdFont-Regular.ttf"
 
@@ -36,88 +37,160 @@ void Panic(int status, const char *format_message, ...) {
   exit(status);
 }
 
+void move_cursor(int direction) {
+  int len = strlen(buffer);
+  int new_cursor_position = 0;
+
+  if (direction > 0) {
+    new_cursor_position = MAX(0, cursor_position - 1);
+  } else {
+    new_cursor_position = MIN(len, cursor_position + 1);
+  }
+
+  if (buffer[new_cursor_position] == '\n') {
+    // TODO: Not working
+    move_cursor(direction);
+    return;
+  }
+
+  if (buffer[new_cursor_position] == '\0') {
+    return;
+  }
+
+  cursor_position = new_cursor_position;
+}
+
 void Type_In_Buffer(char key) { String_Push_Char(&buffer, key); }
 
-int Render_Paragraph(SDL_Renderer *renderer, TTF_Font *font,
-                     const char *wantedText, SDL_Color textColor,
-                     SDL_Color textBackgroundColor, int x, int y) {
+int Render_Text(SDL_Renderer *renderer, TTF_Font *font, const char *wanted_text,
+                SDL_Color text_color, SDL_Color text_background_color, int x,
+                int y) {
   SDL_Texture *text = NULL;
-  SDL_Rect textRect;
+  SDL_Rect text_rect;
 
-  SDL_Surface *textSurface =
-      TTF_RenderText_Shaded(font, wantedText, textColor, textBackgroundColor);
-  if (!textSurface) {
+  SDL_Surface *text_surface = TTF_RenderText_Shaded(
+      font, wanted_text, text_color, text_background_color);
+  if (!text_surface) {
     Panic(1, "Unable to render text surface!\nSDL2_ttf Error: %s\n",
           TTF_GetError());
   }
 
-  text = SDL_CreateTextureFromSurface(renderer, textSurface);
+  text = SDL_CreateTextureFromSurface(renderer, text_surface);
   if (!text) {
     Panic(1, "Unable to create texture from rendered text!\nSDL2 Error: %s\n",
           SDL_GetError());
   }
 
-  textRect.w = textSurface->w;
-  textRect.h = textSurface->h;
+  text_rect.w = text_surface->w;
+  text_rect.h = text_surface->h;
 
-  SDL_FreeSurface(textSurface);
+  SDL_FreeSurface(text_surface);
 
-  textRect.x = x;
-  textRect.y = y;
+  text_rect.x = x;
+  text_rect.y = y;
 
   SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
-  SDL_RenderCopy(renderer, text, NULL, &textRect);
+  SDL_RenderCopy(renderer, text, NULL, &text_rect);
 
   return 1;
 }
 
 int Render_Cursor(SDL_Renderer *renderer, TTF_Font *font, char character, int x,
                   int y) {
-  SDL_Color cursorColor = {0x00, 0x00, 0x00, 0xFF};
-  SDL_Color cursorBackgroundColor = {0xFF, 0xFF, 0xFF, 0xFF};
+  SDL_Color color = {0x00, 0x00, 0x00, 0xFF};
+  SDL_Color background_color = {0xFF, 0xFF, 0xFF, 0xFF};
 
+  // Every 500ms, the cursor will blink
   if (SDL_GetTicks64() % 1000 < 500) {
-    cursorBackgroundColor = (SDL_Color){0xFF, 0xFF, 0xFF, 0xFF};
+    background_color = (SDL_Color){0xFF, 0xFF, 0xFF, 0xFF};
   } else {
-    cursorBackgroundColor = (SDL_Color){0x00, 0x00, 0x00, 0xFF};
+    background_color = (SDL_Color){0x00, 0x00, 0x00, 0xFF};
   }
 
   char *character_string = malloc(2);
   character_string[0] = character;
   character_string[1] = '\0';
 
-  Render_Paragraph(renderer, font, character_string, cursorColor,
-                   cursorBackgroundColor, x, y);
+  Render_Text(renderer, font, character_string, color, background_color, x, y);
 
   return 1;
 }
 
-int Render_Text(SDL_Renderer *renderer, TTF_Font *font,
-                const char *wantedText) {
-  SDL_Color textColor = {0x00, 0x00, 0x00, 0xFF};
-  SDL_Color textBackgroundColor = {0xFF, 0xFF, 0xFF, 0xFF};
+int Render_Buffer(SDL_Renderer *renderer, TTF_Font *font,
+                  const char *buffer_text) {
+  SDL_Color text_color = {0x00, 0x00, 0x00, 0xFF};
+  SDL_Color text_background_color = {0xFF, 0xFF, 0xFF, 0xFF};
 
   int line_number = 0;
+  int line_column = 0;
   char *current_line = "\0";
   char current_character;
 
-  for (int i = 0; (unsigned)i < strlen(wantedText) + 1; i++) {
-    current_character = wantedText[i];
+  for (int i = 0; (unsigned)i < strlen(buffer_text) + 1; i++) {
+    current_character = buffer_text[i];
+    int is_cursor = i == cursor_position;
 
-    if (current_character == '\n') {
+    if (is_cursor) {
       if (strlen(current_line) > 0) {
-        Render_Paragraph(renderer, font, current_line, textColor,
-                         textBackgroundColor, 0, line_number * CHAR_HEIGHT);
+        Render_Text(renderer, font, current_line, text_color,
+                    text_background_color,
+                    (line_column - strlen(current_line)) * CHAR_WIDTH,
+                    line_number * CHAR_HEIGHT);
       }
 
+      Render_Cursor(renderer, font, current_character, line_column * CHAR_WIDTH,
+                    line_number * CHAR_HEIGHT);
+
+      if (buffer_text[i + 1] != '\n') {
+        current_line = "";
+        line_column++;
+      }
+
+      continue;
+    }
+
+    if (current_character == '\0' && strlen(current_line) > 0) {
+      Render_Text(renderer, font, current_line, text_color,
+                  text_background_color,
+                  (line_column - strlen(current_line)) * CHAR_WIDTH,
+                  line_number * CHAR_HEIGHT);
+      continue;
+    }
+
+    if (current_character == '\n' && strlen(current_line) > 0) {
+      Render_Text(renderer, font, current_line, text_color,
+                  text_background_color,
+                  (line_column - strlen(current_line)) * CHAR_WIDTH,
+                  line_number * CHAR_HEIGHT);
       current_line = "";
       line_number++;
-    } else if (current_character != '\0') {
-      String_Push_Char(&current_line, current_character);
-    } else if (strlen(current_line) > 0) {
-      Render_Paragraph(renderer, font, current_line, textColor,
-                       textBackgroundColor, 0, line_number * CHAR_HEIGHT);
+      line_column = 0;
+      continue;
     }
+
+    String_Push_Char(&current_line, current_character);
+    line_column++;
+
+    // if (current_character == '\n') {
+    //   if (strlen(current_line) > 0) {
+    //     Render_Text(renderer, font, current_line, text_color,
+    //                 text_background_color,
+    //                 (line_column - strlen(current_line)) * CHAR_HEIGHT,
+    //                 line_number * CHAR_HEIGHT);
+    //   }
+    //
+    //   current_line = "";
+    //   line_number++;
+    //   line_column = 0;
+    // } else if (current_character != '\0') {
+    //   String_Push_Char(&current_line, current_character);
+    //   line_column++;
+    // } else if (strlen(current_line) > 0) {
+    //   Render_Text(renderer, font, current_line, text_color,
+    //               text_background_color,
+    //               (line_column - strlen(current_line)) * CHAR_HEIGHT,
+    //               line_number * CHAR_HEIGHT);
+    // }
   }
 
   return 1;
@@ -213,6 +286,16 @@ int main(int argc, char *argv[]) {
         Type_In_Buffer('\n');
         break;
       }
+      case SDLK_LEFT: {
+        // cursor_position = MAX(0, cursor_position - 1);
+        move_cursor(1);
+        break;
+      }
+      case SDLK_RIGHT: {
+        // cursor_position = MIN((int)strlen(buffer) - 1, cursor_position + 1);
+        move_cursor(-1);
+        break;
+      }
       default: {
         break;
       }
@@ -224,7 +307,7 @@ int main(int argc, char *argv[]) {
     SDL_RenderClear(renderer);
 
     if (buffer[0] != '\0') {
-      Render_Text(renderer, font, buffer);
+      Render_Buffer(renderer, font, buffer);
     }
 
     SDL_RenderPresent(renderer);

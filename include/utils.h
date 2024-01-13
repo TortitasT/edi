d1e555a4e492b3b5df81429712c1626898734357
Victor Garcia <76071376+TortitasT@users.noreply.github.com>
2024-01-13 21:48:57 +0100

    Globals (#3)
    * feat: globals header file
    
    * feat: globals

--- include/utils.h
+++ include/utils.h
@@ -1,10 +1,12 @@
 #ifndef UTILS_H_INCLUDED
 #define UTILS_H_INCLUDED
 
+#include <SDL_ttf.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+void Panic(int status, const char *format_message, ...);
 void String_Push_Char(char **str, char concat);
 void String_Pop_Char(char **str, int position);
 
